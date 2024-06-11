9e10514bf0a19ef230b04974dbd3962699b62feb
Maxiee <maxieewong@gmail.com>
2024-06-11 23:47:17 +0800

    update

--- lib/python/project_mangement/poetry/poetry.dart
+++ lib/python/project_mangement/poetry/poetry.dart
@@ -10,22 +10,63 @@ class Poetry extends StatelessWidget {
         title: 'Poetry',
         child: C(children: [
           R(children: [
-            Box(title: 'poetry build\nwhl'),
-            Box(title: 'poetry publish\nPyPI'),
+            Box(title: 'poetry build', subtitle: '构建 whl'),
+            Box(
+              title: 'poetry publish',
+              subtitle: '发布 PyPI',
+            ),
+            Box(
+              title: 'poetry run',
+              subtitle: '执行脚本',
+            ),
           ]),
-          Group(
-              title: '包管理',
-              child: R(children: [
-                Box(title: 'poetry add'),
-                Box(title: 'poetry update'),
-                Box(title: 'poetry track\ndeps')
-              ])),
+          R(
+            children: [
+              Group(
+                  title: '包管理',
+                  child: C(children: [
+                    R(children: [
+                      Box(title: 'poetry install', subtitle: '安装依赖'),
+                      Box(
+                        title: 'poetry add',
+                        subtitle: '添加依赖',
+                      ),
+                    ]),
+                    R(children: [
+                      Box(
+                        title: 'poetry update',
+                        subtitle: '更新依赖',
+                      ),
+                      Box(title: 'poetry track', subtitle: '打印依赖关系')
+                    ])
+                  ])),
+              Group(
+                  title: '虚拟环境',
+                  child: C(children: [
+                    Box(
+                      title: 'poetry shell',
+                      subtitle: '进入 venv shell',
+                    ),
+                    Box(
+                      title: 'exit',
+                      subtitle: '退出 venv shell',
+                    ),
+                  ])),
+            ],
+            flexes: [3, 2],
+          ),
           Group(
               title: '工程管理',
               child: R(children: [
-                Box(title: 'poetry new project\n创建工程'),
-                Box(title: 'poetry update'),
-                Box(title: 'poetry track\ndeps')
+                Box(title: 'poetry new project', subtitle: '创建工程'),
+                Box(
+                  title: 'pyproject.toml',
+                  subtitle: '工程配置文件',
+                ),
+                Box(
+                  title: 'poetry init',
+                  subtitle: '初始化已有项目',
+                )
               ]))
         ]));
   }
